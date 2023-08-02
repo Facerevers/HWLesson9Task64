79608f4bf44bea5cd8190589caeb2a2005e79e1f
crazle <crazle@mail.ru>
2023-08-02 13:00:13 +0300

    Числа от N до 1

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string FindNumbers(int numb)
+{
+    if (numb == 1) return numb.ToString();
+    else return numb + ", " + FindNumbers(numb-1);
+}
+
+int GetInput(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int numb = GetInput("Введите число N:");
+Console.Write(FindNumbers(numb));
